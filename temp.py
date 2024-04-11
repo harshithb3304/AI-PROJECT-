0ba77d9690288045319fe595e67f4d279edd03f9
harshithb3304 <harsh03032004@gmail.com>
2024-04-11 12:26:10 +0530

    implemented heuristic

--- temp.py
+++ temp.py
@@ -1,16 +1,20 @@
+import numpy as np
 from copy import deepcopy
+
 
 sudoku_puzzle = [
     [4, 3, 0, 0],
     [0, 0, 0, 0],
-    [0, 0, 2, 1],
+    [0, 0, 2, 0],
     [0, 0, 0, 0]
 ]
 
+sudoku_puzzle = np.array(sudoku_puzzle)
 
 def heuristic():
     current_state = deepcopy(sudoku_puzzle)
     zero_count = [0,0,0,0]
+    zero_count = np.array(zero_count)
 
     for i in range(4):
         for j in range(4):
@@ -27,15 +31,16 @@ def heuristic():
         if zero_count[i] == 0:
             zero_count[i] = 999
 
-    min_value = min(zero_count)
-    for i in range(4):
-        if zero_count[i] == min_value:
-            index = zero_count.index(i)
-            subgrid_row, subgrid_col = (index//2 * 2, index%2 * 2)    
-            for i in range(subgrid_row, subgrid_row + 2):
-                for j in range(subgrid_col, subgrid_col + 2):
-                    print(current_state[i][j], end=" ")
-                print()
+    search_key = min(zero_count)
+    item_index = np.where(zero_count == search_key)[0] 
+    print(item_index)
+    for i in item_index:
+        subgrid_row, subgrid_col = (i//2 * 2, i%2 * 2)    
+        for i in range(subgrid_row, subgrid_row + 2):
+            for j in range(subgrid_col, subgrid_col + 2):
+                print(current_state[i][j], end=" ")
+            print()
+    
     
     
 
